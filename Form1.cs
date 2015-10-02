67f1a4bccbde133eb802dd4e83be65e4c77b6355
AlbearKamoo <mrautran@gmail.com>
2015-10-01 20:52:08 -0700

    Added code comments

--- Form1.cs
+++ Form1.cs
@@ -17,6 +17,9 @@ namespace LLC_Csharp
         {
             InitializeComponent();
 
+            // Dictionary containing all the currently supported languages in Leaugue of Legends
+            // The key represents the language code while the value is a string representing how 
+            // the language would appear in the client language options
             Dictionary<String, String> langList = new Dictionary<string, string>();
             langList.Add("cs_CZ", "Čeština");
             langList.Add("nl_NL", "Deutsch");
@@ -33,6 +36,8 @@ namespace LLC_Csharp
             langList.Add("ru_RU", "Русский");
             langList.Add("tr_TR", "Türkçe");
 
+            // listBox that takes the previuosly defined dictionary as a data source
+            // Allows the user to easily select the preferred language
             listBox1.DataSource = new BindingSource(langList, null);
             listBox1.DisplayMember = "Value";
             listBox1.ValueMember = "Key";
@@ -43,33 +48,43 @@ namespace LLC_Csharp
             FolderBrowserDialog fdialog = new FolderBrowserDialog();
             fdialog.Description = "Please select the Riot Games folder:";
             fdialog.ShowNewFolderButton = false;
-
+            
+            // Sets the default directory to the default folder path provided in the League of Legends installation
             fdialog.SelectedPath = "C:\\Riot Games";
 
             if (fdialog.ShowDialog() == DialogResult.OK)
             {
+                // Sets the text box to contain the user's selected directory
                 textBox1.Text = fdialog.SelectedPath.ToString();
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            // Saves the first part of the necessary file paths by adding the user povided
+            // directory to a string of folders that stays constant throughout 
+            // League of Legends client updates
             string releasePath = textBox1.Text + "\\League of Legends\\RADS\\solutions\\lol_game_client_sln\\releases";
 
-
+            // Exception handling for the provided directory
             try
             {
+                // Gets the contents contained in the previously defined releases directory
                 string[] patchDirectory = Directory.GetDirectories(releasePath);
 
+                // Gets the path of the first folder contained in the releases folder, whose name changes with every
+                // update to the League of Legends client, and adds the last part of the necessary file paths to it.
                 string solutionsFilePath = patchDirectory[0] + "\\deploy\\DATA\\cfg\\defaults\\locale.cfg";
                 string systemFilePath = textBox1.Text + "\\League of Legends\\RADS\\system\\locale.cfg";
-            
+                
+                // Performs the necessary modifications to the text files that dictate the game's language options
                 string[] lines = { "[General]", "LanguageLocaleRegion=" + listBox1.SelectedValue };
                 System.IO.File.WriteAllLines(@solutionsFilePath, lines);
                 System.IO.File.WriteAllText(@systemFilePath, "locale = " + listBox1.SelectedValue);
             }
             catch (DirectoryNotFoundException except)
             {
+                // Handles the case in which the required folders could not be found, and relays this to the user.
                 MessageBox.Show("Incorrect root folder!\t\t", "Directory error", MessageBoxButtons.OK);
             }
 
